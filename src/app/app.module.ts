9a843e0c33776561af8a4912f63c34b085a15a67
vrao-jpg <vasudevrao@gmail.com>
2020-02-07 00:32:37 +0530

    added about

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { homeComponent } from './home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { aboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    homeComponent
+    homeComponent,
+    aboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,52 @@
 import { Component } from '@angular/core';
+import { trigger, transition , style, animate, state, group } from '@angular/animations';
 
 @Component({
   selector: 'header-component',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  animations: [
+    trigger('showDropDown', [
+      state('in', style({
+        'height': '140px', 'opacity': '1', 'visibility': 'visible'
+      })),
+      state('out', style({
+        'height': '0px', 'opacity': '0', 'visibility': 'hidden'
+      })),
+      transition('in => out', [group([
+        animate('300ms ease-in-out', style({
+          'opacity': '0'
+        })),
+        animate('500ms ease-in-out', style({
+          'height': '0px'
+        })),
+        animate('600ms ease-in-out', style({
+          'visibility': 'hidden'
+        })),
+      ])
+      ]),
+      transition('out => in', [group([
+        animate('1ms ease-in-out', style({
+          'visibility': 'visible'
+        })),
+        animate('800ms ease-in-out', style({
+          'height': '140px'
+        })),
+        animate('600ms ease-in-out', style({
+          'opacity': '1'
+        })),
+      ])
+      ])
+    ])
+  ]
 })
 export class HeaderComponent{
   linkList = ['About', 'Career', 'Contact'];
   showMenu : boolean= false;
+  animationState = 'out';
+  toggleDiv() {
+    this.animationState = this.animationState === 'out' ? 'in' : 'out';
+    console.warn(this.animationState);
+    this.showMenu = !this.showMenu;
+  }
 }
